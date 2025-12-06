23cb2e38ca411ce24eee953ff7a810310a2c3ed9
mslxl <i@mslxl.com>
2025-12-06 20:51:36 +0800

    feat: add devtools

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,24 +1,4 @@
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+// react-scan must be the first import
+import 'react-scan/all-environments'
 
-import { createRouter, RouterProvider } from '@tanstack/react-router'
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { routeTree } from './routeTree.gen'
-import './globals.css'
-
-const router = createRouter({ routeTree })
-const queryClient = new QueryClient()
-
-declare module '@tanstack/react-router' {
-  interface Register {
-    router: typeof router
-  }
-}
-
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-    </QueryClientProvider>
-  </StrictMode>,
-)
+import './app'
--- apps/client/src/routes/__root.tsx
+++ apps/client/src/routes/__root.tsx
@@ -1,6 +1,7 @@
 import { SidebarInset, SidebarProvider } from '@memorilo/components/ui/sidebar'
 import { createRootRoute, Outlet } from '@tanstack/react-router'
 import { AppSidebar } from '~/components/app-sidebar'
+import { DebugInfo } from '~/components/debug-info'
 
 export const Route = createRootRoute({
   component: RouteComponent,
@@ -8,11 +9,13 @@ export const Route = createRootRoute({
 
 function RouteComponent() {
   return (
-    <SidebarProvider>
-      <AppSidebar />
-      <SidebarInset>
-        <Outlet />
-      </SidebarInset>
-    </SidebarProvider>
+    <DebugInfo>
+      <SidebarProvider>
+        <AppSidebar />
+        <SidebarInset>
+          <Outlet />
+        </SidebarInset>
+      </SidebarProvider>
+    </DebugInfo>
   )
 }
