00b42369481b482b65f242dad14003d0426a0121
mslxl <i@mslxl.com>
2025-12-26 23:30:34 +0800

    fix(editor): exit codeblock with double enter on end of block. Close #12

--- packages/editor/src/hooks/use-key-down-handler.ts
+++ packages/editor/src/hooks/use-key-down-handler.ts
@@ -1,6 +1,7 @@
 import type { KeyboardEvent } from 'react'
 import { useCallback } from 'react'
 import { useSlateStatic } from 'slate-react'
+import { onCodeblockExit } from './handlers/on-codeblock-exit'
 import { onIndent } from './handlers/on-indent'
 import { onNavigation } from './handlers/on-navigation'
 import { onSoftBreak } from './handlers/on-soft-break'
@@ -8,7 +9,7 @@ import { onSoftBreak } from './handlers/on-soft-break'
 export function useKeyDownHandler() {
   const editor = useSlateStatic()
   return useCallback((event: KeyboardEvent<HTMLDivElement>) => {
-    const handlers = [onIndent, onSoftBreak, onNavigation]
+    const handlers = [onCodeblockExit, onIndent, onSoftBreak, onNavigation]
     for (const handler of handlers) {
       if (handler(event, editor)) {
         return
