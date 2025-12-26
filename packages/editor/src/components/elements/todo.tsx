fabb2b7b1af0a963eb5442b2d689947247a2ef1e
mslxl <i@mslxl.com>
2025-12-27 00:27:32 +0800

    fix(editor): replace todo with new style to avoid inconsist. Fix #13

--- packages/editor/src/components/elements/todo.tsx
+++ packages/editor/src/components/elements/todo.tsx
@@ -1,10 +1,14 @@
 import type { RenderElementProps } from 'slate-react'
 import type { TodoElementType } from '../../slate'
+import { TodoSwitch } from '@memorilo/components/ui/todo-switch'
+import { cn } from '@memorilo/utils'
 import { useCallback } from 'react'
 import { Transforms } from 'slate'
 import { ReactEditor, useSlateStatic } from 'slate-react'
 
 export function Todo(props: RenderElementProps) {
+  const DONE_WITH_LINE_THROUGH = true // TODO: make this a user setting
+
   const editor = useSlateStatic()
   const toggleChecked = useCallback((value: boolean) => {
     const path = ReactEditor.findPath(editor, props.element)
@@ -14,14 +18,22 @@ export function Todo(props: RenderElementProps) {
   return (
     <div {...props.attributes} className="py-1 flex items-start gap-2 leading-6">
       <span contentEditable={false} className="h-lh flex items-center select-none">
-        <input
-          type="checkbox"
-          className="size-4 cursor-pointer accent-blue-300 checked:border-0"
+        <TodoSwitch
           checked={(props.element as TodoElementType).checked}
-          onChange={e => toggleChecked(e.target.checked)}
+          onCheckedChange={toggleChecked}
+          checkedTrackClassName="bg-green-500"
+          uncheckedTrackClassName="bg-blue-300"
+          checkedLabel="DONE"
+          uncheckedLabel="TODO"
+          checkedLabelClassName="text-white"
+          uncheckedLabelClassName="text-slate-900"
+          aria-label="Toggle TODO"
         />
       </span>
-      <div className="flex-1 min-w-0 wrap-break-word">
+      <div className={cn('flex-1 min-w-0 wrap-break-word', {
+        'text-muted-foreground line-through': (props.element as TodoElementType).checked && DONE_WITH_LINE_THROUGH,
+      })}
+      >
         {props.children}
       </div>
     </div>
