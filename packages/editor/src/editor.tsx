915b6cc61bbd513ea27cb1ad1a52294f1a9937db
mslxl <i@mslxl.com>
2026-01-22 01:13:46 +0800

    feat(editor): add uuid for outline node

--- packages/editor/src/editor.tsx
+++ packages/editor/src/editor.tsx
@@ -12,6 +12,7 @@ import Paragraph from '@tiptap/extension-paragraph'
 import Strike from '@tiptap/extension-strike'
 import Text from '@tiptap/extension-text'
 import Underline from '@tiptap/extension-underline'
+import UniqueID from '@tiptap/extension-unique-id'
 
 import { Gapcursor } from '@tiptap/extensions'
 import { EditorContent, useEditor } from '@tiptap/react'
@@ -101,6 +102,11 @@ export function MemoriloEditor({ className, doc, username, ...props }: MemoriloE
         TableExtension,
         EmojiExtension,
         Gapcursor,
+        UniqueID.configure({
+          attributeName: 'uuid',
+          updateDocument: true,
+          types: ['listItem', 'orderedItem', 'taskItem'],
+        }),
         loroSyncExtension,
       ],
     },
